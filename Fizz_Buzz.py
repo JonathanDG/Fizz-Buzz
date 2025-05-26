afca9516cd1058e86fa7017f925fbdf52a31b44f
Jona <jonathandg23@gmail.com>
2025-05-26 12:48:58 -0600

    Added FizzBuzz Program

---
+++ Fizz_Buzz.py
@@ -0,0 +1,19 @@
+
+num = 1
+
+while num < 1000:
+    text =""
+
+    
+    if num %3 == 0:
+        text+="FIZZ"
+    
+    if num %5 == 0:
+        text+="BUZZ"
+    
+    if len(text) >0:
+        print(text)
+    else:
+        print(num)
+
+    num+=1
