b11d00d88c01e375a9e39f90d1a060db850dcc10
Jona <jonathandg23@gmail.com>
2025-05-26 13:14:17 -0600

    Added FizzBuzz2 Program

---
+++ Fizz_Buzz2.py
@@ -0,0 +1,15 @@
+#JONATHAN DURON GONZALEZ   
+num = 1
+
+while num < 1000:
+    text =""
+    if num %15 == 0:
+       print("FIZZBUZZ")
+    elif num %5 == 0:
+        print("BUZZ")
+    elif num %3 == 0:
+        print("FIZZ")
+    else:
+        print(num)
+
+    num+=1
